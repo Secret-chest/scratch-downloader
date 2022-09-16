0fb5c77271bd945750c971323771b4421b132042
vlad <vlad.muntoiu@gmail.com>
2022-09-16 18:54:35 +0300

    Forgot quotes.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='scratch_downloader',
     packages=find_packages(include=["scratch_downloader"]),
-    version='0.1.0',
+    version='1.0.0-rc',
     description='Download Scratch (www.scratch.mit.edu) SB3 files',
     long_description=open('README.md').read(),
     long_description_content_type="text/x-markdown",
