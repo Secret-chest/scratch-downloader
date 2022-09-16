fcf71f37a95dcbd3a3bd14bb96996e70ead118ce
Secret-chest <74449186+Secret-chest@users.noreply.github.com>
2022-09-16 18:34:29 +0300

    I copy-pasted lol

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(include=["scratch_downloader"]),
     version='0.1.0',
     description='Download Scratch (www.scratch.mit.edu) SB3 files',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     author='Secret-chest',
     license='Apache-2.0',
     install_requires=["httpx"],
